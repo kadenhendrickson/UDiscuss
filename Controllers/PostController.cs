4909edb729a2c3c308b314beeb84290fb6f8df93
DAVID PATTERSON <u1207184@umail.utah.edu>
2022-03-30 23:33:44 -0600

    added basic post controller

---
+++ Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UDiscuss.Models;
+
+namespace UDiscuss.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PostController : ControllerBase
+{
+
+    private readonly ILogger<PostController> _logger;
+
+    public PostController(ILogger<PostController> logger)
+    {
+        _logger = logger;
+    }
+
+    // what does this meta tag do?
+    [HttpGet]
+    public IEnumerable<Post> Get()
+    {
+        return null;
+    }
+}
