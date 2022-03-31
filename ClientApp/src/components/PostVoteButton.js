32c84720e8ed02e15b89825f863f90f3b2132ae3
Lucas <LucasKats24@gmail.com>
2022-03-30 23:06:40 -0600

    Created the PostVoteButton (still needs to have a trinagular shape and update the DB).

---
+++ ClientApp/src/components/PostVoteButton.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+/*
+ * Represents an upvote button which user can click to toggle their vote.
+ *  
+ * */
+export class PostVoteButton extends Component {
+    static displayName = PostVoteButton.name;
+
+    constructor(props) {
+        super(props);
+
+        // has two props representing the number of upvotes and if it is currently toggled.
+        this.state = { upvotes: props.upvotes, isToggled: props.isToggled };
+        this.toggleUpvote = this.toggleUpvote.bind(this);
+    }
+
+    /*
+     * Updates the toggle state and vote count from the click
+     * 
+     * TODO update database
+     * 
+     * */
+    toggleUpvote() {
+        if (this.state.isToggled) {
+            this.setState({ upvotes: this.state.upvotes - 1 });
+            this.setState({ isToggled: false });
+        }
+
+        else {
+            this.setState({ upvotes: this.state.upvotes + 1 });
+            this.setState({ isToggled: true });
+        }
+    }
+
+
+
+    // TODO - turn toggle button into a triangle.
+    render() {
+        let button;
+        if (this.state.isToggled)
+            button = <button className="btn btn-danger" onClick={this.toggleUpvote}></button>;
+        else
+            button = <button className="btn btn-primary" onClick={this.toggleUpvote}></button>;
+        return (
+            <div>
+
+                {button}
+                <p aria-live="polite">{this.state.upvotes}</p>
+            </div>
+        );
+    }
+}
